392cb4ff12cd1a30c33353f7b1c49ab657be77ba
basel7ayman <basel.elsa3di@gmail.com>
2024-11-20 23:14:46 +0200

    last changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 const strings = [
-  "Test string 1",
-  "Test string 2",
-  "Test string 3",
-  "Test string 4",
-  "Test string 5",
-  "Test string 6",
-  "Test string 7",
-  "Test string 8",
-  "Test string 9"
+  `“Be yourself; everyone else is already taken.” <br>
+  ― Oscar Wilde`,
+  `“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.” <br>
+  ― Marilyn Monroe`,
+  `“So many books, so little time.” <br>
+  ― Frank Zappa`,
+  `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” <br>
+  ― Albert Einstein`,
+  `“A room without books is like a body without a soul.” <br>
+  ― Marcus Tullius Cicero`,
+  `“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.” <br>
+  ― Bernard M. Baruch`,
+  `“You know you're in love when you can't fall asleep because reality is finally better than your dreams.” <br>
+  ― Dr. Seuss`
 ];
 
+let lastIndex = -1; 
+
 function displayRandomString() {
-  const randomIndex = Math.floor(Math.random() * strings.length);
-  const randomString = strings[randomIndex];
-  document.getElementById("output").textContent = randomString;
+  let randomIndex = lastIndex;
+
+  do {
+    randomIndex = Math.floor(Math.random() * strings.length);
+  } while (randomIndex === lastIndex);
+  
+  lastIndex = randomIndex;
+  document.getElementById("output").innerHTML = strings[randomIndex];
 }
